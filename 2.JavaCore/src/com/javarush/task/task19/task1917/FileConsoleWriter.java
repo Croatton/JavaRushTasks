04536b38083ed7c3e6b8199f597a5a3850a5a5e5
admin <Croatton@gmail.com>
2017-03-15 18:12:51 +0200

    12

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -0,0 +1,75 @@
+package com.javarush.task.task19.task1917;
+
+/* 
+Свой FileWriter
+*/
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileConsoleWriter {
+    private FileDescriptor fd;
+    private File file;
+    private boolean append;
+    private String fileName;
+    private FileWriter fileWriter;
+
+
+    public FileConsoleWriter(String fileName) throws IOException {
+        fileWriter = new FileWriter(fileName);
+    }
+
+    public FileConsoleWriter(String fileName, boolean append) throws IOException {
+        fileWriter = new FileWriter(fileName, append);
+    }
+
+    public FileConsoleWriter(File file) throws IOException {
+        fileWriter = new FileWriter(file);
+    }
+
+    public FileConsoleWriter(File file, boolean append) throws IOException {
+        fileWriter = new FileWriter(file, append);
+    }
+
+    public FileConsoleWriter(FileDescriptor fd) {
+        fileWriter = new FileWriter(fd);
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        for (int i = off; i < off + len; i++) {
+            System.out.print(cbuf[i]);
+        }
+    }
+
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.print(c);
+    }
+
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.print(str);
+    }
+
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.println(str.substring(off, len + off));
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.println(cbuf);
+    }
+
+    public void close() throws IOException {
+        fileWriter.close();
+    }
+}
