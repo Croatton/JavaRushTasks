f7a74a436a56c8a3e96eee6ec058c52c3bf93b87
admin <Croatton@gmail.com>
2017-03-16 13:53:10 +0200

    12

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -15,18 +15,18 @@ import java.util.TreeMap;
 public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String openTag = "<" + args[0];//
-        String closeTag = "</" + args[0] + ">";
+        String openTag = "<" + args[0];// открывающий
+        String closeTag = "</" + args[0] + ">";// закрывающий
         BufferedReader fileReader = new BufferedReader(new FileReader(reader.readLine()));
         reader.close();
         StringBuilder sb = new StringBuilder();
         while (fileReader.ready()) {
-            sb.append(fileReader.readLine());
+            sb.append(fileReader.readLine());// считываем все строки в стрингбилдер
         }
         fileReader.close();
 
-        int openTagIndex = sb.indexOf(openTag);
-        int closeTagIndex = sb.indexOf(closeTag);
+        int openTagIndex = sb.indexOf(openTag);// индекс открывающего
+        int closeTagIndex = sb.indexOf(closeTag);// индекс закрывающего
 
         Map<Integer, Boolean> tagIndexesMap = new TreeMap<>();
         Stack<Integer> tagStack = new Stack<>();
@@ -44,9 +44,11 @@ public class Solution {
 
         for (Map.Entry<Integer, Boolean> entry : tagIndexesMap.entrySet()) {
             if (entry.getValue()) {
-                tagStack.push(entry.getKey());
+                tagStack.push(entry.getKey());// индексы всех открывающих тэгов в стэк
             } else {
                 finalIndexes.put(tagStack.pop(), entry.getKey());
+                /*если entry.getValue() == false , забираем из стэка последний попавший туда индекс(индеск начала)
+                * и добавляем индекс конца*/
             }
         }
         for (Map.Entry<Integer, Integer> entry : finalIndexes.entrySet())
